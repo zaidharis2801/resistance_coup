87df7e930c5fc323d984e1cc9c0edfc91a737d2c
zaidharis2801 <s.haris.22868@khi.iba.edu.pk>
2024-07-17 15:54:17 +0500

    added playagent

--- src/handler/game_handler.py
+++ src/handler/game_handler.py
@@ -359,7 +359,16 @@ class ResistanceCoupGameHandler:
          
         # Choose an action to perform
         target_action, target_player = self._action_phase(players_without_current)
+        rational_knowledge_dict_str = json.dumps(self._knowledges[self._current_player_index].to_dict())
+        
+        inputs_play2 = {
+            "rational_knowledge": rational_knowledge_dict_str,
+            "intermediate_steps": []
+        }
 
+        out = self._play_agents[self._current_player_index].get_result(inputs_play2)
+        print(out["agent_out"])
+        input("buffer: ")
 
         move_dict["action"] = str(target_action.action_type.value)
         if target_player:
